da7a0c6781ac711643458a55018fae3c9a7b548d
subodhpoudel12 <subodhpoudel1000@gmail.com>
2024-01-16 12:40:28 +0545

    changed the index and example page

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,11 +1,8 @@
-import { Container } from '@edx/paragon';
+// import { Container } from '@edx/paragon';
 
 const ExamplePage = () => (
   <main>
-    <Container className="py-5">
-      <h1>Example Page</h1>
-      <p>Hello world!</p>
-    </Container>
+    <p>Hello world!</p>
   </main>
 );
 
